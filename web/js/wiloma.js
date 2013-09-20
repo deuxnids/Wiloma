9e92edb1d8569194e1d04e3158f40696e09872af
Denis <denism@student.ethz.ch>
2013-09-20 09:17:41 +0200

    add dataTables -> still some issues

--- web/js/wiloma.js
+++ web/js/wiloma.js
@@ -1,6 +1,23 @@
 
 $(document).ready(function() {
 	 
+
+
+		
+		$.extend( $.fn.dataTableExt.oStdClasses, {
+		    "sWrapper": "dataTables_wrapper form-inline"
+		} );
+		
+		$('#list').dataTable(
+				{        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+					"oLanguage": {
+						"sLengthMenu": "_MENU_ éléments"
+							
+					}});
+
+	
+
+	
 	$( "#wlm_Operationbundle_Operationtype_code" ).keypress(function( event ) {
 			console.log("sd");
 		});
